da8697cdbb802cbe6e1700bd8ab159f4a7901bb4
wangzhenqi <754371430@qq.com>
2015-11-04 11:39:17 +0800

    new project
    蓝牙项目

---
+++ BlueToothBLE/BlueToothBLE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueToothBLE
+//
+//  Created by klicen on 15/11/4.
+//  Copyright © 2015年 beimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlueToothBLE/BlueToothBLE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlueToothBLE
+//
+//  Created by klicen on 15/11/4.
+//  Copyright © 2015年 beimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
